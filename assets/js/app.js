a113558c3fb5541a6fea9a1c7212f2bb979f7e7f
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-04-20 15:29:17 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
+
+const app = new Vue(
+    {
+
+    el: '#app',
+    
+        data: {
+        saluti: ' hello VueJs',
+           
+            
+    }
+   
+    }
+)
---
+++ bonus/assets/js/app.js
@@ -0,0 +1,11 @@
+const app = new Vue(
+    {
+        el: '#app',
+        
+        data: {
+            saluti: 'hello VueJs',
+            img: '../bonus/assets/img/vuejs.jpeg'
+        }
+    }
+
+)
